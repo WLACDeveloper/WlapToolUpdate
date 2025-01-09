7cd03688addc9e0c4db6ea255874f9591b4707a6
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-01-09 19:57:46 +0300

    Add files via upload

---
+++ update.py
@@ -0,0 +1,7 @@
+version = 1.00
+branch = 'main'
+#Если будет версии патча отличаться, то можно скачать обнову
+patch = 1
+repository = 'https://github.com/WLACDeveloper/Wlap-FlashTool/tree/main'
+#Если версия ниже минимальной, то начнется скачивание полной версии программыы
+update_packages=['WlaplFlashTool']
