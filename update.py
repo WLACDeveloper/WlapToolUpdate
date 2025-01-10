fcab24347e5dea70f3c185e242efb49fb8f360b7
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-01-10 17:43:39 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 version = 1.00
 branch = 'main'
 #Если будет версии патча отличаться, то можно скачать обнову
-patch = 4
+patch = 6
 repository = 'https://raw.githubusercontent.com/WLACDeveloper/Wlap-FlashTool/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=['settings/version.py','image/black/ADB.png','image/black/android.jpg','image/black/approve_custom_load.png','image/black/arch.png','image/black/attetion.png', 'settings/window_and_objects.py', 'main.py','image/black/bootloader.png','image/black/close.png','image/black/closes.png','image/black/debian.png','image/black/fastbootd.png','image/black/folder_music.png','image/black/language.png','image/black/lock.png','image/black/music.png','image/black/product.png','image/black/recovery.png','image/black/right.png','image/black/search_gsi.png','image/black/system.png','image/black/twrp.png','image/black/unlock.png','image/black/update.png','image/black/volume_off.png','image/black/volume_on.png','image/black/windows.png','image/black/wipe_data.png']
+update_packages=['main.py', 'settings/window_and_objects.py', 'settings/version.py']
