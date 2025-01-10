90b393d516f05e877cef8d93236d8de97deef667
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-01-10 09:25:51 +0300

    Add files via upload

---
+++ update (2).py
@@ -0,0 +1,7 @@
+version = 1.00
+branch = 'main'
+#Если будет версии патча отличаться, то можно скачать обнову
+patch = 2
+repository = 'https://raw.githubusercontent.com/WALMFAST/walmfast/refs/heads/'
+#Если версия ниже минимальной, то начнется скачивание полной версии программыы
+update_packages=['settings/version.py', 'script/updater.py']
