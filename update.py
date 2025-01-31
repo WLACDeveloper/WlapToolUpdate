cac91d8d5c4ff88dc22a2bb31659e4cf4e888136
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-01-31 14:07:55 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 version = 2.00
 branch = 'main'
 #Если будет версии патча отличаться, то можно скачать обнову
-patch = 0
+patch = 1
 repository = 'https://raw.githubusercontent.com/WLACDeveloper/Wlap-FlashTool/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=['image/driver/ss.png']
+update_packages=['']
