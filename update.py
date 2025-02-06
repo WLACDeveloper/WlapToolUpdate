c37713c772bcc8e09aa5c6311676a8e17098abcf
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-02-06 21:09:14 +0300

    Update update.py

--- update.py
+++ update.py
@@ -4,4 +4,4 @@ branch = 'main'
 patch = 2
 repository = 'https://raw.githubusercontent.com/WLACDeveloper/Wlap-FlashTool/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=['main.py','Userdata/version.py','Userdata/windows_and_objects.py','script/imageload.py','image/black/programfuncupdt.jpg','image/black/coreupdt.jpg','image/black/bugfixupdt.jpg','image/black/uiupdtd.jpg']
+update_packages=['main.py','Userdata/version.py','Userdata/window_and_objects.py','script/imageload.py','image/black/programfuncupdt.jpg','image/black/coreupdt.jpg','image/black/bugfixupdt.jpg','image/black/uiupdtd.jpg']
